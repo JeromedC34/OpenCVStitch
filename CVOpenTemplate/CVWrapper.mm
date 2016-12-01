a22854d61648b162dd669c7edc8087e977a84d6b
Jerome <jdc@free.fr>
2016-12-01 16:46:18 +0100

    Add 'using namespace cv'

--- CVOpenTemplate/CVWrapper.mm
+++ CVOpenTemplate/CVWrapper.mm
@@ -16,6 +16,9 @@
 #import "opencv2/highgui/highgui.hpp"
 #import "opencv2/calib3d/calib3d.hpp"
 #import "opencv2/nonfree/nonfree.hpp"
+#import "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
 
 @implementation CVWrapper
 
@@ -39,7 +42,7 @@
         NSLog (@"imageArray is empty");
         return 0;
     }
-    cv::vector<cv::Mat> matImages;
+    vector<Mat> matImages;
     
     for (id image in imageArray) {
         if ([image isKindOfClass: [UIImage class]]) {
@@ -47,38 +50,38 @@
              All images taken with the iPhone/iPa cameras are LANDSCAPE LEFT orientation. The  UIImage imageOrientation flag is an instruction to the OS to transform the image during display only. When we feed images into openCV, they need to be the actual orientation that we expect them to be for stitching. So we rotate the actual pixel matrix here if required.
              */
             UIImage* rotatedImage = [image rotateToImageOrientation];
-            cv::Mat matImage = [rotatedImage CVMat3];
+            Mat matImage = [rotatedImage CVMat3];
             NSLog (@"matImage: %@",image);
             matImages.push_back(matImage);
         }
     }
     NSLog (@"stitching...");
-    cv::Mat stitchedMat = stitch (matImages);
+    Mat stitchedMat = stitch (matImages);
     UIImage* result =  [UIImage imageWithCVMat:stitchedMat];
     return result;
 }
 
 + (UIImage*) check2Images:(UIImage*)inputImage1 image2:(UIImage*)inputImage2;
 {
-    cv::Mat img_object = [inputImage1 CVMat3];
-    cv::Mat img_scene  = [inputImage2 CVMat3];
+    Mat img_object = [inputImage1 CVMat3];
+    Mat img_scene  = [inputImage2 CVMat3];
 
     //-- Step 1: Detect the keypoints using SURF Detector
     int minHessian = 400;
-    cv::SurfFeatureDetector detector( minHessian );
-    std::vector<cv::KeyPoint> keypoints_object, keypoints_scene;
+    SurfFeatureDetector detector( minHessian );
+    std::vector<KeyPoint> keypoints_object, keypoints_scene;
     detector.detect( img_object, keypoints_object );
     detector.detect( img_scene, keypoints_scene );
     
     //-- Step 2: Calculate descriptors (feature vectors)
-    cv::SurfDescriptorExtractor extractor;
-    cv::Mat descriptors_object, descriptors_scene;
+    SurfDescriptorExtractor extractor;
+    Mat descriptors_object, descriptors_scene;
     extractor.compute( img_object, keypoints_object, descriptors_object );
     extractor.compute( img_scene, keypoints_scene, descriptors_scene );
     
     //-- Step 3: Matching descriptor vectors using FLANN matcher
-    cv::FlannBasedMatcher matcher;
-    std::vector< cv::DMatch > matches;
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
     matcher.match( descriptors_object, descriptors_scene, matches );
     double max_dist = 0; double min_dist = 100;
     
@@ -91,20 +94,20 @@
     printf("-- Min dist : %f \n", min_dist );
     
     //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
-    std::vector< cv::DMatch > good_matches;
+    std::vector< DMatch > good_matches;
     
     for( int i = 0; i < descriptors_object.rows; i++ ) {
         if( matches[i].distance < 3*min_dist ) {
             good_matches.push_back( matches[i]);
         }
     }
-    cv::Mat img_matches;
+    Mat img_matches;
     drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
-                good_matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1),
-                cv::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
     //-- Localize the object
-    std::vector<cv::Point2f> obj;
-    std::vector<cv::Point2f> scene;
+    std::vector<Point2f> obj;
+    std::vector<Point2f> scene;
     
     for( int i = 0; i < good_matches.size(); i++ )
     {
@@ -112,20 +115,20 @@
         obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
         scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
     }
-    cv::Mat H = findHomography( obj, scene, CV_RANSAC );
+    Mat H = findHomography( obj, scene, CV_RANSAC );
     //-- Get the corners from the image_1 ( the object to be "detected" )
-    std::vector<cv::Point2f> obj_corners(4);
+    std::vector<Point2f> obj_corners(4);
     obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
     obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
-    std::vector<cv::Point2f> scene_corners(4);
+    std::vector<Point2f> scene_corners(4);
     
     perspectiveTransform( obj_corners, scene_corners, H);
     
     //-- Draw lines between the corners (the mapped object in the scene - image_2 )
-    line( img_matches, scene_corners[0] + cv::Point2f( img_object.cols, 0), scene_corners[1] + cv::Point2f( img_object.cols, 0), cvScalar(0, 255, 0), 4 );
-    line( img_matches, scene_corners[1] + cv::Point2f( img_object.cols, 0), scene_corners[2] + cv::Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
-    line( img_matches, scene_corners[2] + cv::Point2f( img_object.cols, 0), scene_corners[3] + cv::Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
-    line( img_matches, scene_corners[3] + cv::Point2f( img_object.cols, 0), scene_corners[0] + cv::Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), cvScalar(0, 255, 0), 4 );
+    line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
     
 //    //-- Show detected matches
 //    imshow( "Good Matches & Object detection", img_matches );
