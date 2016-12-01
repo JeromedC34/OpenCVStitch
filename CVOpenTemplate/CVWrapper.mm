4d7d09df6db18334fe826a2b23b441ee2c2f1265
Jerome <jdc@free.fr>
2016-12-01 12:21:35 +0100

    Start removing useless stuff

--- CVOpenTemplate/CVWrapper.mm
+++ CVOpenTemplate/CVWrapper.mm
@@ -11,6 +11,11 @@
 #import "stitching.h"
 #import "UIImage+Rotate.h"
 
+#import "opencv2/core/core.hpp"
+#import "opencv2/features2d/features2d.hpp"
+#import "opencv2/highgui/highgui.hpp"
+#import "opencv2/calib3d/calib3d.hpp"
+#import "opencv2/nonfree/nonfree.hpp"
 
 @implementation CVWrapper
 
@@ -33,9 +38,9 @@
     if ([imageArray count]==0){
         NSLog (@"imageArray is empty");
         return 0;
-        }
+    }
     cv::vector<cv::Mat> matImages;
-
+    
     for (id image in imageArray) {
         if ([image isKindOfClass: [UIImage class]]) {
             /*
@@ -53,72 +58,53 @@
     return result;
 }
 
-
 + (UIImage*) check2Images:(UIImage*)inputImage1 image2:(UIImage*)inputImage2;
 {
     cv::Mat img_object = [inputImage1 CVMat3];
     cv::Mat img_scene  = [inputImage2 CVMat3];
+
     //-- Step 1: Detect the keypoints using SURF Detector
     int minHessian = 400;
-//    SurfFeatureDetector detector( minHessian );
-/*
-    Mat img_object = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-    Mat img_scene = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
-    
-    if( !img_object.data || !img_scene.data )
-    { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
-    
-    //-- Step 1: Detect the keypoints using SURF Detector
-    int minHessian = 400;
-    
-    SurfFeatureDetector detector( minHessian );
-    
-    std::vector<KeyPoint> keypoints_object, keypoints_scene;
-    
+    cv::SurfFeatureDetector detector( minHessian );
+    std::vector<cv::KeyPoint> keypoints_object, keypoints_scene;
     detector.detect( img_object, keypoints_object );
     detector.detect( img_scene, keypoints_scene );
     
     //-- Step 2: Calculate descriptors (feature vectors)
-    SurfDescriptorExtractor extractor;
-    
-    Mat descriptors_object, descriptors_scene;
-    
+    cv::SurfDescriptorExtractor extractor;
+    cv::Mat descriptors_object, descriptors_scene;
     extractor.compute( img_object, keypoints_object, descriptors_object );
     extractor.compute( img_scene, keypoints_scene, descriptors_scene );
     
     //-- Step 3: Matching descriptor vectors using FLANN matcher
-    FlannBasedMatcher matcher;
-    std::vector< DMatch > matches;
+    cv::FlannBasedMatcher matcher;
+    std::vector< cv::DMatch > matches;
     matcher.match( descriptors_object, descriptors_scene, matches );
-    
     double max_dist = 0; double min_dist = 100;
     
     //-- Quick calculation of max and min distances between keypoints
-    for( int i = 0; i < descriptors_object.rows; i++ )
-    { double dist = matches[i].distance;
+    for( int i = 0; i < descriptors_object.rows; i++ ) { double dist = matches[i].distance;
         if( dist < min_dist ) min_dist = dist;
         if( dist > max_dist ) max_dist = dist;
     }
-    
     printf("-- Max dist : %f \n", max_dist );
     printf("-- Min dist : %f \n", min_dist );
     
     //-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
-    std::vector< DMatch > good_matches;
+    std::vector< cv::DMatch > good_matches;
     
-    for( int i = 0; i < descriptors_object.rows; i++ )
-    { if( matches[i].distance < 3*min_dist )
-    { good_matches.push_back( matches[i]); }
+    for( int i = 0; i < descriptors_object.rows; i++ ) {
+        if( matches[i].distance < 3*min_dist ) {
+            good_matches.push_back( matches[i]);
+        }
     }
-    
-    Mat img_matches;
+    cv::Mat img_matches;
     drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
-                good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
-                vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
-    
+                good_matches, img_matches, cv::Scalar::all(-1), cv::Scalar::all(-1),
+                cv::vector<char>(), cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
     //-- Localize the object
-    std::vector<Point2f> obj;
-    std::vector<Point2f> scene;
+    std::vector<cv::Point2f> obj;
+    std::vector<cv::Point2f> scene;
     
     for( int i = 0; i < good_matches.size(); i++ )
     {
@@ -126,26 +112,24 @@
         obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
         scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
     }
-    
-    Mat H = findHomography( obj, scene, CV_RANSAC );
-    
+    cv::Mat H = findHomography( obj, scene, CV_RANSAC );
     //-- Get the corners from the image_1 ( the object to be "detected" )
-    std::vector<Point2f> obj_corners(4);
+    std::vector<cv::Point2f> obj_corners(4);
     obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
     obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
-    std::vector<Point2f> scene_corners(4);
+    std::vector<cv::Point2f> scene_corners(4);
     
     perspectiveTransform( obj_corners, scene_corners, H);
     
     //-- Draw lines between the corners (the mapped object in the scene - image_2 )
-    line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
-    line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-    line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
-    line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[0] + cv::Point2f( img_object.cols, 0), scene_corners[1] + cv::Point2f( img_object.cols, 0), cvScalar(0, 255, 0), 4 );
+    line( img_matches, scene_corners[1] + cv::Point2f( img_object.cols, 0), scene_corners[2] + cv::Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[2] + cv::Point2f( img_object.cols, 0), scene_corners[3] + cv::Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
+    line( img_matches, scene_corners[3] + cv::Point2f( img_object.cols, 0), scene_corners[0] + cv::Point2f( img_object.cols, 0), cvScalar( 0, 255, 0), 4 );
     
-    //-- Show detected matches
-    imshow( "Good Matches & Object detection", img_matches );
-*/
+//    //-- Show detected matches
+//    imshow( "Good Matches & Object detection", img_matches );
+    return [UIImage imageWithCVMat:img_matches];
 }
 
 @end
--- CVOpenTemplate/MyOpenCV.swift
+++ CVOpenTemplate/MyOpenCV.swift
@@ -11,8 +11,9 @@ import UIKit
 
 class MyOpenCV {
     func main() -> UIImage {
-        let img1:UIImage = UIImage(named: "pano_19_16_mid.jpg")!
-        let img2:UIImage = UIImage(named: "pano_19_25_mid.jpg")!
+        let img1:UIImage = UIImage(named: "trolltunga0.jpg")!
+        let img2:UIImage = UIImage(named: "trolltunga.jpg")!
+//        let img2:UIImage = UIImage(named: "pano_19_25_mid.jpg")!
 //        CVWrapper.processImageWithOpenCV(img1)
 //        CVWrapper.processImage(withOpenCV: img1)
         let img3:UIImage = CVWrapper.check2Images(img1, image2: img2)
